c7d6206e80d0389fa8604044c2ba0ae2723df863
marcelaruiz12 <144807294+marcelaruiz12@users.noreply.github.com>
2023-09-14 09:15:38 -0500

    Add files via upload

---
+++ facadedani/src/fachada/CPU.java
@@ -0,0 +1,13 @@
+package fachada;
+
+public class CPU {
+public void freeze(){
+    System.out.println("CPU:  FREEZING.....");
+}
+public void execute(){
+    System.out.println("CPU: Executing...");
+}
+public void jum(){
+    System.out.println("CPU:  Jumping...");
+}
+}
